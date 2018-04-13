fd561c3eca7a4b8731faa2a800ee9669e804d2ff
asdx <xasdx@tuta.io>
2018-04-13 16:15:01 +0200

    Implement the execution of assertions of each property defined in specs

--- index.js
+++ index.js
@@ -6,14 +6,44 @@ let forEachProperty = (obj, f) => {
   }
 }
 
-let unit = targetUnit => {
-  return {
-    specs: specifications => {
-      forEachProperty(specifications, (property, cases) => {
-        if (!targetUnit[property]) { throw new Error(`Non-existing property ${property}`) }
-      })
+let io = () => {
+  return f => {
+    /* do assertions */
+    return {
+      success: true,
+      type: "io",
+      meta: {
+        input: true,
+        output: true
+      }
     }
   }
 }
 
-module.exports = { unit }
+let processSpecifications = (targetUnit, specifications) => {
+
+  let specsRunResults = []
+
+  forEachProperty(specifications, (property, testCases) => {
+    let functionality = targetUnit[property]
+    if (!functionality) { throw new Error(`Non-existing property '${property}'`) }
+    if (typeof functionality !== "function") { throw new Error(`'${property}' must be a function`) }
+
+    if (Array.isArray(testCases)) {
+      let propCaseResults = testCases.map(testCase => testCase(functionality))
+      specsRunResults.push({ property, results: propCaseResults })
+    } else {
+      throw new Error("Test cases must be specified in an array")
+    }
+  })
+
+  return specsRunResults
+}
+
+let unit = targetUnit => {
+  return {
+    specs: specs => processSpecifications(targetUnit, specs)
+  }
+}
+
+module.exports = { unit, io }
--- test/test.js
+++ test/test.js
@@ -1,15 +1,29 @@
 let { expect } = require("chai")
-let { unit } = require("..")
+let { unit, io } = require("..")
 
 let m = {
-  addOne: n => n + 1
+  addOne: n => n + 1,
+  subtractTwo: n => n - 2
 }
 
 module.exports = {
   
   "testr": {
+
     "verifiesInterface": () => {
       expect(() => unit(m).specs({ addTwo: true })).to.throw(/non-existing/i)
+    },
+
+    "executesAssertions": () => {
+      let results = unit(m).specs({
+        addOne: [io(1, 2), io(9, 10)],
+        subtractTwo: [io(6, 4)]
+      })
+      expect(results).to.have.lengthOf(2)
+      expect(results[0].property).to.be.equal("addOne")
+      expect(results[0].results).to.have.lengthOf(2)
+      expect(results[1].property).to.be.equal("subtractTwo")
+      expect(results[1].results).to.have.lengthOf(1)
     }
   }
 }
