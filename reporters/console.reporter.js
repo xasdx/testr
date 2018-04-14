6c9b3a4bc231f409cf85887e626c67fad5397464
asdx <xasdx@tuta.io>
2018-04-14 14:07:00 +0200

    Update default console reporter to be more colorful

--- reporters/console.reporter.js
+++ reporters/console.reporter.js
@@ -1,20 +1,53 @@
+let { is } = require("../util")
+
 let TYPE_IO = "io"
 
 let isMatcher = obj => obj.matcherType !== undefined
 
+let COLOR = {
+  YELLOW: "\x1b[33m%s\x1b[0m",
+  GREEN: "\x1b[32m%s\x1b[0m",
+  RED: "\x1b[31m%s\x1b[0m"
+}
+
+let colorize = function () {
+  return [...arguments].map(token => is.array(token) ? token[0].replace("%s", token[1]) : token).join(" ")
+}
+
+let logColored = function () { console.log(colorize(...arguments)) }
+
+let logSuccess = result => logColored(
+  [COLOR.GREEN, " +"],
+  result.moduleType,
+  "#",
+  "input",
+  [COLOR.GREEN, JSON.stringify(result.meta.input)],
+  "outputs",
+  [COLOR.GREEN, JSON.stringify(result.meta.output.actual)]
+)
+
+let logFailure = result => {
+  let expected = isMatcher(result.meta.output.expected) ? result.meta.output.expected.toString() : result.meta.output.expected
+  logColored(
+    [COLOR.RED, " -"],
+    result.moduleType,
+    "#",
+    "input",
+    [COLOR.RED, JSON.stringify(result.meta.input)],
+    "should output",
+    `${expected},`,
+    "but got",
+    [COLOR.RED, JSON.stringify(result.meta.output.actual)]
+  )
+}
+
 let reporter = testResults => {
   testResults.forEach(testResult => {
-    console.log(`-- ${testResult.property}`)
+    logColored([COLOR.YELLOW, "--"], testResult.property)
     testResult.results.forEach(result => {
       switch (result.moduleType) {
         case TYPE_IO:
-          if (result.success) {
-            console.log(` + ${result.moduleType} # input ${result.meta.input} outputs ${result.meta.output.actual}`)
-          } else {
-            let expected = isMatcher(result.meta.output.expected) ? result.meta.output.expected.toString() : result.meta.output.expected
-            console.log(` - ${result.moduleType} # input ${result.meta.input} should output ${expected} (got ${result.meta.output.actual})`)
-          }
-          break
+          return result.success ? logSuccess(result) : logFailure(result)
         default:
           throw new Error(`[reporter] result type ${result.moduleType} is not supported`)
       }
